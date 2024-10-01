a6cafdc60dadd135ccc9984eac53bb583e60791f
Urs Baumann <github@m.ubaumann.ch>
2024-10-01 21:05:17 +0000

    Use Upsert, create devices

--- infrahub/init_data.py
+++ infrahub/init_data.py
@@ -5,6 +5,7 @@ create_prefix = """
 mutation {
   IpamIPPrefixCreate(data: {
     prefix: {value: "10.0.0.0/24"},
+    description: {value: "mgmt subnet"},
     member_type: {value: "address"},
   })
   {
@@ -18,8 +19,8 @@ mutation {
 
 create_resource_manager = """
 mutation {
-  CoreIPAddressPoolCreate(data: {
-    name: {value: "My IP address pool"},
+  CoreIPAddressPoolUpsert(data: {
+    name: {value: "mgmt IP address pool"},
     default_address_type: {value: "IpamIPAddress"},
     default_prefix_length: {value: 32},
     resources: [{id: "%s"}],
@@ -34,36 +35,12 @@ mutation {
 }
 """
 
-
-
-get_devices = """
-query MyQuery {
-  NetworkDevice {
-    edges {
-      node {
-        hfid
-        name {
-          value
-        }
-        description {
-          value
-        }
-      }
-    }
-  }
-}
-"""
-
-
-
 allocate_ip = """
 mutation {
   IPAddressPoolGetResource(
     data: {
-      id: "<id or resource pool>",
-      data: {
-        description: "my first allocated ip"
-      }
+      id: "%s",
+      identifier: "mgmt gw",
     }
   )
   {
@@ -101,20 +78,68 @@ mutation {
 }
 """
 
+create_devices = [
+    """
+mutation MyMutation1 {
+  NetworkDeviceUpsert(
+    data: {name: {value: "switch01"}, status: {value: "active"} description: {value: "My awesome switch"}}
+  ) {
+    ok
+  }
+}""",
+    """
+mutation MyMutation2 {
+  NetworkDeviceUpsert(
+    data: {name: {value: "switch02"}, status: {value: "active"} description: {value: "My second awesome switch"}}
+  ) {
+    ok
+  }
+}
+""",
+]
+
+get_devices = """
+query MyQuery {
+  NetworkDevice {
+    edges {
+      node {
+        hfid
+        name {
+          value
+        }
+        description {
+          value
+        }
+      }
+    }
+  }
+}
+"""
+
+
 def main():
     # Needs environment variable `INFRAHUB_API_TOKEN`
     client = InfrahubClientSync(address="http://localhost:8000")
-    
+
     # Create prefix
     result = client.execute_graphql(create_prefix)
     prefix_id = result["IpamIPPrefixCreate"]["object"]["id"]
     print(prefix_id)
-    
-    
+
     # Create resource pool for IPs
     result = client.execute_graphql(create_resource_manager % prefix_id)
+    pool_id = result["CoreIPAddressPoolUpsert"]["object"]["id"]
+    print(pool_id)
+
+    # Allocate mgmt GW IP
+    result = client.execute_graphql(allocate_ip % pool_id)
     print(result)
 
-    print(use_pool_in_node % "TESTTESTETEST")
+    # Create Devices
+    for device_mutation in create_devices:
+      result = client.execute_graphql(device_mutation)
+      print(result)
+
+
 if __name__ == "__main__":
     main()
